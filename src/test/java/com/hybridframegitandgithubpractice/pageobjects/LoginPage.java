515476f5c1ca625e5613b00cab355c865cd2fa53
Anita <anitakarve93@gmail.com>
2022-05-14 09:56:41 +0530

    updated LoginTC. java in Pageobject. added the function

--- src/test/java/com/hybridframegitandgithubpractice/pageobjects/LoginPage.java
+++ src/test/java/com/hybridframegitandgithubpractice/pageobjects/LoginPage.java
@@ -1,6 +1,10 @@
 package com.hybridframegitandgithubpractice.pageobjects;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
 
 public class LoginPage {
 	
@@ -9,6 +13,52 @@ public class LoginPage {
 	public LoginPage(WebDriver driver)
 	{
 		this.driver= driver;
+		PageFactory.initElements(driver, this);
 	}
-
+	
+	@FindBy(name ="txtUsername")
+	WebElement txtUsername;
+	
+	@FindBy(name ="txtPassword")
+	WebElement txtPassword;
+	
+	@FindBy(name ="Submit")
+	WebElement loginBtn;
+	
+	public void setUserName(String username) {
+		try {
+			txtUsername.clear();
+			txtUsername.sendKeys(username);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public void setPassword(String pass) {
+		try {
+			txtPassword.clear();
+			txtPassword.sendKeys(pass);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void clickOnLogin() {
+		try {
+			loginBtn.click();
+			String exp_title = "OrangeHRM";
+			String act_title = driver.getTitle();	
+			if(exp_title.equals(act_title)) {
+				Assert.assertTrue(true);
+				
+			}else
+			{
+				Assert.assertTrue(false);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			
+		}
+	}
 }
--- src/test/java/com/hybridframegitandgithubpractice/testcases/LoginTC001.java
+++ src/test/java/com/hybridframegitandgithubpractice/testcases/LoginTC001.java
@@ -1,12 +1,31 @@
 package com.hybridframegitandgithubpractice.testcases;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
+
+import com.hybridframegitandgithubpractice.pageobjects.LoginPage;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class LoginTC001 {
 
+	String url = "https://opensource-demo.orangehrmlive.com/index.php";
+	String username = "Admin";
+	String password = "admin123";
+
 	@Test
-	public void verifyLoginFunctionality()
-	{
+	public void verifyLoginFunctionality() {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		 driver.get(url);
+		LoginPage LoginPage = new LoginPage(driver);
+		
+		LoginPage.setUserName(username);
+		LoginPage.setPassword(password);
+		LoginPage.clickOnLogin();
+		
+ 
 		
 	}
 }
